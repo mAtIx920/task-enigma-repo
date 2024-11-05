af4ae4b4e17f3c600924093fa6ccf0b94a7c0eb1
Mateusz Rosiński <mateusz.rosinski@horus.com.pl>
2024-11-05 23:20:48 +0100

    commit task

---
+++ src/test/java/org/rest/restapp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rest.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
